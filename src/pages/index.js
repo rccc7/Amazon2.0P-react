c5bbab0b1d11f52284a563b0f13cf6084ff5ce3e
RCCC <rcristhiancc@gmail.com>
2023-03-27 00:54:48 -0400

    Updated the title of the page to Amazn CLONE 2.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
-        <title>Amazon 2.0</title>
+        <title>Amazn CLONE 2.0</title>
       </Head>
 
       {/* <h1>Hey RCCC Followers 😎🔥🍫</h1> */}
